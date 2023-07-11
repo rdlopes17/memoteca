0485a405db8d0ceed8317ce630798c5f8356a4c9
rdlopes17 <rauldlopes@gmail.com>
2023-07-11 09:07:39 -0300

    desenvolvimento-property binding

--- src/app/componentes/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CriarPensamentosComponent implements OnInit {
 
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: ''
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
