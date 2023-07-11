605d29587683c07443aece116815fba0dbd7376c
rdlopes17 <rauldlopes@gmail.com>
2023-07-11 09:47:35 -0300

    desenv-two way data bainding

--- src/app/componentes/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
@@ -11,7 +11,7 @@ export class CriarPensamentosComponent implements OnInit {
     id: '1',
     conteudo: 'Aprendendo Angular',
     autoria: 'Dev',
-    modelo: ''
+    modelo: 'modelo1'
   }
 
   constructor() { }
