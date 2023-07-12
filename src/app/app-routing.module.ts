45380ca1ee24417c5f541e146fd72bb95487f5b1
rdlopes17 <rauldlopes@gmail.com>
2023-07-12 09:38:36 -0300

    desenv- config routing parte 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentosComponent } from './componentes/pensamentos/criar-pensamentos/criar-pensamentos.component';
+import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'listarPensamento',
+    pathMatch:'full'
+  },
+  {
+    path: 'criarPensamento',
+    component: CriarPensamentosComponent
+  },
+  {
+    path: 'listarPensamento',
+    component: ListarPensamentoComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
