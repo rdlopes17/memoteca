3109b7785e7b5fa0624768264851387b85d001e2
rdlopes17 <rauldlopes@gmail.com>
2023-07-17 09:31:37 -0300

    desenv-*ngClasss style dinamico

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -17,6 +17,11 @@ export class ListarPensamentoComponent implements OnInit {
       conteudo: 'Minha propriedade e decorada com @input()',
       autoria: 'Componete filho',
       modelo: 'modelo1'
+    },
+    {
+      conteudo: 'Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Diuretics paradis num copo é motivis de denguis. Atirei o pau no gatis, per gatis num morreus. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis. Sapien in monti palavris qui num significa nadis i pareci latim.',
+      autoria: 'Teste de style dinamico',
+      modelo: 'modelo2'
     }
   ];
 
