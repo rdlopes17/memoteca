98ff812b13078e1e1a61f6283870595fa0334642
rdlopes17 <rauldlopes@gmail.com>
2023-08-08 10:27:47 -0300

    desenv-Router/navigate

--- src/app/componentes/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-criar-pensamentos',
@@ -15,17 +16,21 @@ export class CriarPensamentosComponent implements OnInit {
     modelo: 'modelo1'
   }
 
-  constructor(private service: PensamentoService) { }
+  constructor(
+    private service: PensamentoService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
   criarPensamento() {
-    this.service.criar(this.pensamento).subscribe()
+    this.service.criar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento'])
+    })
   }
 
   cancelar() {
-    alert('Ação Cancelada')
+    this.router.navigate(['/listarPensamento'])
   }
 
 
