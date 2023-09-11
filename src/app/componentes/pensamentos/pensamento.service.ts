6519d177989e573ae70c4a24087d5eee5f70ac7c
rdlopes17 <rauldlopes@gmail.com>
2023-09-11 09:45:40 -0300

    desenv-httpClient pt 6 Editar/Pendamentos-ID

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -19,6 +19,11 @@ export class PensamentoService {
     return this.http.post<Pensamento>(this.API, pensamento)
   }
 
+  ediar(pensamento: Pensamento): Observable<Pensamento> {
+    const url = `${this.API}/${pensamento.id}`
+    return this.http.put<Pensamento>(url, pensamento)
+  }
+
   excluir(id: number): Observable<Pensamento> {
     const url = `${this.API}/${id}`
     return this.http.delete<Pensamento>(url)
