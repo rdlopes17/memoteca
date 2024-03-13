ac5d42d2c3d09d63b0d79fe7d0b55b3edfe2f734
rdlopes17 <rauldlopes@gmail.com>
2024-03-13 08:40:16 -0300

    desenv-Validators pt1

--- src/app/componentes/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamentos',
@@ -23,16 +23,19 @@ export class CriarPensamentosComponent implements OnInit {
 
   ngOnInit(): void {
     this.formulario = this.formBuilder.group({
-      conteudo:['Formulário reativo'],
-      autoria:['autoria'],
+      conteudo:['',[ Validators.required]],
+      autoria:['',[ Validators.required]],
       modelo: ['modelo1']
     })
   }
 
   criarPensamento() {
-    this.service.criar(this.formulario.value).subscribe(() => {
-      this.router.navigate(['/listarPensamento'])
-    })
+    console.log('log de validacao', this.formulario);
+    if (this.formulario.valid) {
+      this.service.criar(this.formulario.value).subscribe(() => {
+        this.router.navigate(['/listarPensamento'])
+      })
+    }
   }
 
   cancelar() {
