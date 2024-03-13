df268109fc689171fd586ffafda16655250b6902
rdlopes17 <rauldlopes@gmail.com>
2024-03-13 08:57:51 -0300

    desenv-Validators pt2 - Validators.compose

--- src/app/componentes/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
@@ -23,14 +23,14 @@ export class CriarPensamentosComponent implements OnInit {
 
   ngOnInit(): void {
     this.formulario = this.formBuilder.group({
-      conteudo:['',[ Validators.required]],
-      autoria:['',[ Validators.required]],
+      conteudo:['',Validators.compose([ Validators.required, Validators.pattern(/(.|\s)*\S(.|\s)*/)])],
+      autoria:['',Validators.compose([ Validators.required, Validators.minLength(3)])],
       modelo: ['modelo1']
     })
   }
 
   criarPensamento() {
-    console.log('log de validacao', this.formulario);
+    console.log('log de validacao', this.formulario.status);
     if (this.formulario.valid) {
       this.service.criar(this.formulario.value).subscribe(() => {
         this.router.navigate(['/listarPensamento'])
