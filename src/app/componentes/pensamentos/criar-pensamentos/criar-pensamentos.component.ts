7ada44a5e2da411966f5d20e8858a4bd167fe0f4
rdlopes17 <rauldlopes@gmail.com>
2024-08-16 10:00:17 -0300

    desenv-validators pt4- validação do botao salvar

--- src/app/componentes/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
@@ -7,12 +7,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-criar-pensamentos',
   templateUrl: './criar-pensamentos.component.html',
-  styleUrls: ['./criar-pensamentos.component.css']
+  styleUrls: ['./criar-pensamentos.component.css'],
 })
 export class CriarPensamentosComponent implements OnInit {
-
-
-
   formulario!: FormGroup;
 
   constructor(
@@ -23,24 +20,39 @@ export class CriarPensamentosComponent implements OnInit {
 
   ngOnInit(): void {
     this.formulario = this.formBuilder.group({
-      conteudo:['',Validators.compose([ Validators.required, Validators.pattern(/(.|\s)*\S(.|\s)*/)])],
-      autoria:['',Validators.compose([ Validators.required, Validators.minLength(3)])],
-      modelo: ['modelo1']
-    })
+      conteudo: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(/(.|\s)*\S(.|\s)*/),
+        ]),
+      ],
+      autoria: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(3)]),
+      ],
+      modelo: ['modelo1'],
+    });
   }
 
   criarPensamento() {
     console.log(this.formulario.get('autoria')?.errors);
     if (this.formulario.valid) {
       this.service.criar(this.formulario.value).subscribe(() => {
-        this.router.navigate(['/listarPensamento'])
-      })
+        this.router.navigate(['/listarPensamento']);
+      });
     }
   }
 
   cancelar() {
-    this.router.navigate(['/listarPensamento'])
+    this.router.navigate(['/listarPensamento']);
   }
 
-
+  habilitarBotao(): string {
+    if (this.formulario.valid) {
+      return 'botao';
+    } else {
+      return 'botao__desabilitado';
+    }
+  }
 }
