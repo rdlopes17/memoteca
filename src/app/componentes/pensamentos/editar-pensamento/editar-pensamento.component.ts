6a6886bd6141f152821409653ba3ae42c63b2add
rdlopes17 <rauldlopes@gmail.com>
2024-08-19 10:28:28 -0300

    desenv-validators pt6- desafio editar

--- src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
+++ src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
@@ -2,42 +2,67 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { PensamentoService } from '../pensamento.service';
 import { Pensamento } from './../pensamento';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { minusculoValidator } from '../criar-pensamentos/minusculoValidator';
 
 @Component({
   selector: 'app-editar-pensamento',
   templateUrl: './editar-pensamento.component.html',
-  styleUrls: ['./editar-pensamento.component.css']
+  styleUrls: ['./editar-pensamento.component.css'],
 })
 export class EditarPensamentoComponent implements OnInit {
-
   pensamento: Pensamento = {
     id: 0,
     conteudo: '',
     autoria: '',
-    modelo: ''
-  }
+    modelo: '',
+  };
+
+  formulario!: FormGroup;
 
   constructor(
     private service: PensamentoService,
     private router: Router,
-    private route: ActivatedRoute) {
- }
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id')
+    const id = this.route.snapshot.paramMap.get('id');
     this.service.buscarPorId(parseInt(id!)).subscribe((pensamento) => {
-      this.pensamento = pensamento
-    })
+      this.formulario = this.formBuilder.group({
+        id: [pensamento.id],
+        conteudo: [
+          pensamento.conteudo,
+          Validators.compose([
+            Validators.required,
+            Validators.pattern(/(.|\s)*\S(.|\s)*/),
+          ]),
+        ],
+        autoria: [
+          pensamento.autoria,
+          Validators.compose([Validators.required, Validators.minLength(3),minusculoValidator]),
+        ],
+        modelo: [pensamento.modelo],
+      });
+    });
   }
 
   editarPensamento() {
     this.service.ediar(this.pensamento).subscribe(() => {
-      this.router.navigate(['/listarPensamento'])
-    })
-   }
-
-  cancelar() {
-    this.router.navigate(['/listarPensamento'])
+      this.router.navigate(['/listarPensamento']);
+    });
   }
 
+  cancelar() {
+    this.router.navigate(['/listarPensamento']);
+  }
+
+  habilitarBotao(): string {
+    if (this.formulario.valid) {
+      return 'botao';
+    } else {
+      return 'botao__desabilitado';
+    }
+  }
 }
