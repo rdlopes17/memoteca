7f18daf3295d5f830967adf099cf80076fdc0708
rdlopes17 <rauldlopes@gmail.com>
2024-08-27 09:42:02 -0300

    desenv-paginacao pt2-botao de carregamento

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -5,19 +5,27 @@ import { PensamentoService } from '../pensamento.service';
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
-  styleUrls: ['./listar-pensamento.component.css']
+  styleUrls: ['./listar-pensamento.component.css'],
 })
 export class ListarPensamentoComponent implements OnInit {
-
   listaPensamentos: Pensamento[] = [];
   paginaAtual: number = 1;
+  haMaisPensamentos: boolean = true;
 
-  constructor(private service: PensamentoService) { }
+  constructor(private service: PensamentoService) {}
 
   ngOnInit(): void {
     this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
-      this.listaPensamentos = listaPensamentos
-    })
+      this.listaPensamentos = listaPensamentos;
+    });
   }
 
+  carregarMaisPensamentos() {
+    this.service.listar(++this.paginaAtual).subscribe((listaPensamentos) => {
+      this.listaPensamentos.push(...listaPensamentos);
+      if (!listaPensamentos.length) {
+        this.haMaisPensamentos = false;
+      }
+    });
+  }
 }
