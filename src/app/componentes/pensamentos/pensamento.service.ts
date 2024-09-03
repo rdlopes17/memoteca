74bae26a3a351e55cc800b078e7c3cf329df11c2
rdlopes17 <rauldlopes@gmail.com>
2024-09-03 10:11:17 -0300

    desenv-filtro pt1-keyup/criacao do metodo de pesquisa

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -10,11 +10,15 @@ export class PensamentoService {
 
   constructor(private http: HttpClient) {}
 
-  listar(pagina: number): Observable<Pensamento[]> {
+  listar(pagina: number, filtro: string): Observable<Pensamento[]> {
     const itesPorPagina = 3;
     let params = new HttpParams()
       .set('_page', pagina)
       .set('_limit', itesPorPagina);
+    
+    if (filtro.trim().length > 2) {
+      params = params.set("q", filtro)
+    }
     // return this.http.get<Pensamento[]>(this.API); retorno simples
     return this.http.get<Pensamento[]>(this.API,{params})
   }
