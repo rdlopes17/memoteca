e3c24ab13f114edeb0637740b6845f44cb56d038
rdlopes17 <rauldlopes@gmail.com>
2024-09-19 09:19:06 -0300

    desenv-favoritos pt4-pesquisa e paginacao dos favoritos com refatoracao do metodo listar

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -10,7 +10,11 @@ export class PensamentoService {
 
   constructor(private http: HttpClient) {}
 
-  listar(pagina: number, filtro: string): Observable<Pensamento[]> {
+  listar(
+    pagina: number,
+    filtro: string,
+    favoritos: boolean
+  ): Observable<Pensamento[]> {
     const itesPorPagina = 3;
     let params = new HttpParams()
       .set('_page', pagina)
@@ -19,22 +23,32 @@ export class PensamentoService {
     if (filtro.trim().length > 2) {
       params = params.set('q', filtro);
     }
+
+    if (favoritos) {
+      params = params.set('favorito', true);
+    }
     // return this.http.get<Pensamento[]>(this.API); retorno simples
     return this.http.get<Pensamento[]>(this.API, { params });
   }
 
-  listarPensamentosFavoritos(pagina: number, filtro: string): Observable<Pensamento[]>{
-    const itesPorPagina = 3;
-    let params = new HttpParams()
-      .set('_page', pagina)
-      .set('_limit', itesPorPagina)
-      .set('favorito', true)
+  // Refatoramos a funcao listar pensamentos para que a mesma pudesse listar os favoritos,
+  //evitando a necessidade de replicar codigo.
 
-    if (filtro.trim().length > 2) {
-      params = params.set('q', filtro);
-    }
-    return this.http.get<Pensamento[]>(this.API, { params });
-  }
+  // listarPensamentosFavoritos(
+  //   pagina: number,
+  //   filtro: string
+  // ): Observable<Pensamento[]> {
+  //   const itesPorPagina = 3;
+  //   let params = new HttpParams()
+  //     .set('_page', pagina)
+  //     .set('_limit', itesPorPagina)
+  //     .set('favorito', true);
+
+  //   if (filtro.trim().length > 2) {
+  //     params = params.set('q', filtro);
+  //   }
+  //   return this.http.get<Pensamento[]>(this.API, { params });
+  // }
 
   criar(pensamento: Pensamento): Observable<Pensamento> {
     return this.http.post<Pensamento>(this.API, pensamento);
